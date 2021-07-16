37e867b4efed9150eb23714340d3f5548420aa81
William Blocker <willb@mavice.com>
2021-07-16 01:59:52 -0400

    feat(profile/jitsi): adding Profile page. Adding Jitsi functionality

---
+++ src/components/Forms/EditProfileForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <BForm @submit="onSubmit">
+      <BFormGroup
+        id="input-group-1"
+        label="Email"
+        label-for="input-1"
+        description="Please enter an email address"
+      >
+        <BFormInput
+          id="input-1"
+          v-model="form.email"
+          placeholder="ex. john@domain.com"
+          type="email"
+          required
+        ></BFormInput>
+      </BFormGroup>
+
+      <BFormGroup
+        id="input-group-2"
+        label="Password"
+        label-for="input-2"
+        description="Please enter a password"
+      >
+        <BFormInput
+          id="input-2"
+          v-model="form.password"
+          max="22"
+          type="password"
+          required
+        ></BFormInput>
+      </BFormGroup>
+      <BButton type="submit" variant="primary">Submit</BButton>
+    </BForm>
+  </div>
+</template>
+
+<script>
+import { BForm, BFormInput, BFormGroup, BButton } from "bootstrap-vue";
+import { mapActions } from "vuex";
+
+export default {
+  name: "LoginWithEmailForm",
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    ...mapActions("User", ["loginUser"]),
+    onSubmit(event) {
+      event.preventDefault();
+      this.loginUser(this.form);
+      this.$bvModal.hide("loginWithEmailModal");
+      this.$router.replace({
+        name: "portal",
+      });
+    },
+  },
+  components: {
+    BForm,
+    BFormInput,
+    BFormGroup,
+    BButton,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Modals/EditProfileModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <BModal id="createRoomModal" title="New Room" hide-footer>
+    <CreateRoomForm></CreateRoomForm>
+  </BModal>
+</template>
+
+<script>
+import { BModal } from "bootstrap-vue";
+import CreateRoomForm from "../Forms/CreateRoomForm";
+
+export default {
+  name: "CreateRoomModal",
+  components: {
+    CreateRoomForm,
+    BModal,
+  },
+};
+</script>
+
+<style scoped></style>
