85bc60b83afc7a615c331dc9e26770d148ae611c
William Blocker <willb@mavice.com>
2021-07-16 16:24:30 -0400

    feat(mentees): adding mentees view

---
+++ src/components/CardGroups/RecommendedMenteesCardGroup.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="recommended-mentors">
+    <slot>Mentors Recommended For You</slot>
+    <BRow class="mt-3">
+      <template v-for="(mentor, mentorId) in getMentors">
+        <BCol md="4" v-if="mentorId !== getCurrentUser.id" :key="mentorId">
+          <UserCard :userId="mentorId" :mentor="mentor"></UserCard>
+        </BCol>
+      </template>
+    </BRow>
+  </div>
+</template>
+
+<script>
+import { BRow, BCol } from "bootstrap-vue";
+import UserCard from "../Cards/UserCard";
+import { mapGetters } from "vuex";
+export default {
+  name: "RecommendedMentorsCardGroup",
+  computed: {
+    ...mapGetters("Mentors", ["getMentors"]),
+    ...mapGetters("User", ["getCurrentUser"]),
+  },
+  components: {
+    UserCard,
+    BRow,
+    BCol,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Portal/Mentees.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mentors">
+    <h2>Mentors</h2>
+
+    <RecommendedMentorsCardGroup></RecommendedMentorsCardGroup>
+  </div>
+</template>
+
+<script>
+import RecommendedMentorsCardGroup from "../../components/CardGroups/RecommendedMentorsCardGroup";
+
+export default {
+  name: "Mentors",
+  components: { RecommendedMentorsCardGroup },
+};
+</script>
+
+<style scoped></style>
